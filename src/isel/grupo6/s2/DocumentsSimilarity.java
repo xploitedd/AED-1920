75c6909ab176378ed0c905c57aa72bb0dfc21bb7
Ricardo Margalhau <kryfor@outlook.com>
2019-11-13 15:05:29 +0000

    second problem done

---
+++ src/isel/grupo6/s2/DocumentsSimilarity.java
@@ -0,0 +1,112 @@
+package isel.grupo6.s2;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class DocumentsSimilarity {
+
+    private static final HashMap<String, Integer> file1 = new HashMap<>();
+    private static final HashMap<String, Integer> file2 = new HashMap<>();
+
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Usage: java -jar documentsSimilarity.jar <file1> <file2>");
+            return;
+        }
+
+        loadFile(args[0], file1);
+        loadFile(args[1], file2);
+
+        Scanner sc = new Scanner(System.in);
+        label:
+        for ( ; ; ) {
+            System.out.print("> ");
+            if (sc.hasNext()) {
+                long tstart = System.currentTimeMillis();
+                String cmd = sc.next();
+
+                switch (cmd) {
+                    case "exit":
+                        break label;
+                    case "allWords":
+                    case "aw":
+                        allWords();
+                        break;
+                    case "wordsWithTheSameOccurrence":
+                    case "wso":
+                        wordsWithTheSameOccurrence(sc.nextInt());
+                        break;
+                    case "similarity":
+                    case "s":
+                        similarity();
+                        break;
+                    default:
+                        if (sc.hasNext()) sc.next();
+                        System.out.println("Command not available. Available Commands:");
+                        System.out.println("allWords - list all words that are at least in one file.");
+                        System.out.println("wordsWithTheSameOccurrence k - list words that are in both files with k occurrences.");
+                        System.out.println("similarity - returns the similarity degree between the files.");
+                        break;
+                }
+
+                long took = System.currentTimeMillis() - tstart;
+                System.out.println("Took: " + took / 1000.0 + " seconds");
+            }
+        }
+    }
+
+    private static void loadFile(String fileName, HashMap<String, Integer> map) {
+        try (Scanner sc = new Scanner(new FileInputStream(fileName))) {
+            while (sc.hasNext()) {
+                String w = sc.next();
+                Integer count = map.get(w);
+                count = (count != null ? count + 1 : 1);
+                map.put(w, count);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void allWords() {
+        for (String k : file1.keySet()) {
+            if (!file2.containsKey(k))
+                System.out.println(k);
+        }
+
+        for (String k : file2.keySet()) {
+            if (!file1.containsKey(k))
+                System.out.println(k);
+        }
+    }
+
+    private static void wordsWithTheSameOccurrence(int k) {
+        if (k > 0) {
+            for (String key : file1.keySet()) {
+                int v2 = file2.getOrDefault(key, 0);
+                int total = file1.get(key) + v2;
+                if (v2 != 0 && total == k)
+                    System.out.println(key);
+            }
+        }
+    }
+
+    private static void similarity() {
+        int similarity = 0;
+        for (String k : file1.keySet()) {
+            int v1 = file1.get(k);
+            Integer v2 = file2.getOrDefault(k, 0);
+            if (v1 != v2) ++similarity;
+        }
+
+        for (String k : file2.keySet()) {
+            if (!file1.containsKey(k)) ++similarity;
+        }
+
+        System.out.println("Similarity between files is " + similarity);
+        System.out.println("Maximum words: " + (file1.size() + file2.size()));
+    }
+
+}
